e5766ef8e2191d5d36c12add54b82fdb6107475c
fadi abuaita <fadi.abuaita.01@outlook.com>
2021-04-19 04:58:43 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public float Multiply(float a, float b) => a * b;
+
+        public float Divide(float a, float b) => a / b;
+    }
+}
---
+++ FunctionalityTestBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator_Test
+{
+    [TestClass]
+    public class FunctionalityTests
+    {
+        [TestMethod]
+        public void Test_Multiply_Result()
+        {
+            Calculator.Calculator calculator = new Calculator.Calculator();
+            Assert.AreEqual(calculator.Multiply(2, 4), 8);
+        }
+
+        [TestMethod]
+        public void Test_Divide_Result()
+        {
+            Calculator.Calculator calculator = new Calculator.Calculator();
+            Assert.AreEqual(calculator.Divide(4, 2), 2);
+        }
+
+    }
+}
